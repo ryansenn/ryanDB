999a439b4482c35366678a8632fcb74bf93ea361
ryan <ryansen87@gmail.com>
2025-03-27 16:05:59 -0400

    separation of concern

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ryanDB
+
+go 1.21.0
--- main.go
+++ main.go
@@ -1,53 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"sync"
 )
 
-type Store struct {
-	data map[string]string
-	mu   sync.RWMutex
+func get(w http.ResponseWriter, r *http.Request) {
+
 }
 
-func (s *Store) Get(w http.ResponseWriter, r *http.Request) {
-	key := r.URL.Query().Get("key")
-	s.mu.RLock()
-	value, ok := s.data[key]
-	s.mu.RUnlock()
-	if !ok {
-		http.Error(w, "Key not found", http.StatusNotFound)
-		return
-	}
-	json.NewEncoder(w).Encode(map[string]string{"value": value})
-}
+func put(w http.ResponseWriter, r *http.Request) {
 
-func (s *Store) Put(w http.ResponseWriter, r *http.Request) {
-	var req map[string]string
-	json.NewDecoder(r.Body).Decode(&req)
-	key, value := req["key"], req["value"]
-	s.mu.Lock()
-	s.data[key] = value
-	s.mu.Unlock()
-	w.WriteHeader(http.StatusOK)
-}
-
-func (s *Store) Delete(w http.ResponseWriter, r *http.Request) {
-	key := r.URL.Query().Get("key")
-	s.mu.Lock()
-	delete(s.data, key)
-	s.mu.Unlock()
-	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
-	store := &Store{data: make(map[string]string)}
-	http.HandleFunc("/get", store.Get)
-	http.HandleFunc("/put", store.Put)
-	http.HandleFunc("/delete", store.Delete)
+	http.HandleFunc("GET", get)
+	http.HandleFunc("PUT", put)
 
-	log.Println("Server on :8080")
+	log.Println("Sever on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
