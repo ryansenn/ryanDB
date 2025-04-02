50b63125016f2ee46c05cade522a2911cf27e470
ryan <ryansen87@gmail.com>
2025-04-02 18:45:10 -0400

    command append log

--- core/log.go
+++ core/log.go
@@ -1,1 +1,44 @@
-package log
+package core
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type Command struct {
+	Op    string
+	Key   string
+	Value string
+}
+
+type Logger struct {
+	file *os.File
+}
+
+func newLogger(id string) *Logger {
+	path := "g" + id
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &Logger{file: f}
+}
+
+func (l *Logger) append(command *Command) error {
+	data, err := json.Marshal(command)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data = append(data, '\n')
+
+	if _, err := l.file.Write(data); err != nil {
+		return err
+	}
+
+	return l.file.Sync()
+}
